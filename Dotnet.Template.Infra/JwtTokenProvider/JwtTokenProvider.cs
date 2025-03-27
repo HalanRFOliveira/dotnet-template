2c47a10e1daf49ca0eff37e78314ed4fd7981f39
AzureAD\HalanRobsonFerreirad <holiveira@deserv.com.br>
2025-03-26 22:07:39 -0300

    Added files for email service and jwt token provider

---
+++ Dotnet.Template.Infra/JwtTokenProvider/JwtTokenProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Dotnet.Template.Infra.JwtTokenProvider
+{
+    public class JwtTokenProvider(IConfiguration configuration) : IJwtTokenProvider
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public string GenerateJwtToken(TokenData user, int expiresIn)
+        {
+            var secretKey = _configuration.GetValue<string>("SecretKey");
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var roles = new List<string>(); // user access roles if you going to use admin levels roles
+            var claims = new ClaimsIdentity(
+                [
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    // Put here more claims if necessary
+                ]);
+            foreach (var role in roles)
+            {
+                claims.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                NotBefore = DateTime.Now,
+                Expires = DateTime.Now.AddDays(expiresIn),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
---
+++ Dotnet.Template.Infra/JwtTokenProvider/TokenData.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet.Template.Infra.JwtTokenProvider
+{
+    public class TokenData
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
